13c49808216b8d3e5ffd31bb4c0218930b6d7835
Justin <jutnhbr@gmail.com>
2023-01-15 00:39:45 +0100

    [+] Adjusted Config (filter, scan duration, default values)

--- config/config.py
+++ config/config.py
@@ -6,20 +6,20 @@ NET=True
 MQTT_START=True
 
 #----WIFI CONNECTION CONFIG----#
-SSID=constants.SSID_HOME
-NETWORK_KEY=constants.NETWORK_PASSWORD_HOME
+SSID=constants.SSID_HTW
+NETWORK_KEY=constants.NETWORK_PASSWORD_HTW
 
 #----MQTT CLIENT CONFIG----#
-BROKER_ADDRESS=constants.BROKER_ADDR
+BROKER_ADDRESS=constants.BROKER_ADDR_HTW
 CLIENT_NAME=constants.CLIENT_NAME
 
 #----SCANNER CONFIG----#
 
-# Time in sec between each scan
-TIME_BETWEEN_SCANS=30
+# Time in sec between each scan (Default 300s = 5min)
+TIME_BETWEEN_SCANS=300
 
 # Scan Duration in ms
-SCAN_DURATION=2000
+SCAN_DURATION=3000
 
 # Delay in sec after each scan result
 SCAN_DELAY=0.2
@@ -31,7 +31,7 @@ ACTIVE_SCAN=True
 FILTER_ADDR_TYPE="ANY"
 
 # Only Include Devices with a higher RSSI (0 for no filter)
-FILTER_RSSI=-85
+FILTER_RSSI=-100
 
 # True = Filters Duplicates while Scanning (Highly Recommended)
 FILTER_DUPLICATES=True
@@ -43,4 +43,4 @@ FILTER_CONNECTABLE=False
 LOGGING=True
 
 # True = Transfers Data via MQTT after Scan
-MQTT=True                    
+MQTT=True            
